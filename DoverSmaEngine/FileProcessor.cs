78e1f6554dbfe9c393a885aa613110b0932afb9c
jkermond-gmail <jkermond@gmail.com>
2018-07-09 18:26:59 -0400

    added (Principal) processor to process MUlitrow offerings and Flows

--- DoverSmaEngine/FileProcessor.cs
+++ DoverSmaEngine/FileProcessor.cs
@@ -155,7 +155,7 @@ namespace DoverSmaEngine
                     ProcessOfferingsDataSingleRow(Path.Combine(mFilepath, mOAFF_legg));
                     break;
                 case "Principal":
-                    //ProcessOfferingsDataMultiRow1(Path.Combine(mFilepath, mOAFF_prin));
+                    ProcessOfferingsDataMultiRow(Path.Combine(mFilepath, mOAFF_prin));
                     break;
                 case "Allianz":
                     ProcessOfferingsDataSingleRow(Path.Combine(mFilepath, mOAFF_alli));
@@ -242,7 +242,7 @@ namespace DoverSmaEngine
             string valueParsed = "";
             string colName = "";
             string tableName = " SmaOfferings ";
-            string logFuncName = "ProcessOfferingsData: ";
+            string logFuncName = "ProcessOfferingsDataSingleRow: ";
 
 
             int currentRowCount = 1; // Since csv file has a header set row to 1, data starts in row 2
@@ -332,16 +332,15 @@ namespace DoverSmaEngine
                         cmd.Parameters["@" + colName].Value = valueParsed;
 
                         colName = "CsvFileRow";
-                        valueParsed = ParseColumn(row, colName);
                         if (addCount == 0) cmd.Parameters.Add("@" + colName, SqlDbType.Int);
                         cmd.Parameters["@" + colName].Value = currentRowCount; 
 
                         cmd.CommandText =
                             "insert into " + tableName +
                                 "(AssetManager, SponsorFirm, AdvisoryPlatform, SmaStrategy, SmaProductType, TampRIAPlatform," +
-                                " SponsorFirmId, MorningstarStrategyId, MorningstarClass, MorningstarClassId, TotalAccounts, CsvFileRow) " +
+                                " SponsorFirmId, MorningstarStrategyId, MorningstarClass, MorningstarClassId, ManagerClass, TotalAccounts, CsvFileRow) " +
                             "Values (@AssetManager, @SponsorFirm, @AdvisoryPlatform, @SmaStrategy, @SmaProductType, @TampRIAPlatform, " +
-                                    "@SponsorFirmId, @MorningstarStrategyId, @MorningstarClass, @MorningstarClassId, @TotalAccounts, @CsvFileRow)";
+                                    "@SponsorFirmId, @MorningstarStrategyId, @MorningstarClass, @MorningstarClassId, @ManagerClass, @TotalAccounts, @CsvFileRow)";
                         cmd.ExecuteNonQuery();
                         addCount += 1;
                     }
@@ -364,6 +363,152 @@ namespace DoverSmaEngine
                 }
                 finally
                 {
+                }
+            }
+            LogHelper.WriteLine(logFuncName + "Rows Processed " + currentRowCount);
+            LogHelper.WriteLine(logFuncName + "Rows Added " + addCount);
+            LogHelper.WriteLine(logFuncName + filePath + " finished");
+        }
+
+        // This routine was wriiten to handle Principals multi row format
+        public void ProcessOfferingsDataMultiRow(string filePath)
+        {
+            SqlCommand cmd = null;
+            string sqlSelect = "";
+            string sqlWhere = "";
+            string valueParsed = "";
+            string colName = "";
+            string tableName = " SmaOfferings ";
+            string logFuncName = "ProcessOfferingsDataMultiRow: ";
+            string sponsorFirm = "";
+            string smaStrategy = "";
+            string smaProductType = "";
+            string morningstarStrategyId = "";
+            string morningstarClass = "";
+
+            int currentRowCount = 1; // Since csv file has a header set row to 1, data starts in row 2
+            int addCount = 0;
+
+            LogHelper.WriteLine(logFuncName + filePath + " started");
+
+            DataTable dt = ReadCsvIntoTable(filePath);
+
+            // Principal does not provide the following key fields so they are removed
+            //  and AdvisoryPlatform = @AdvisoryPlatform  
+            //  and TampRIAPlatform = @TampRIAPlatform
+            sqlSelect = "select count(*) from" + tableName;
+            sqlWhere = @"where AssetManager = @AssetManager and SponsorFirm = @SponsorFirm  and SmaStrategy = @SmaStrategy and 
+                        SmaProductType = @SmaProductType and ManagerClass = @ManagerClass";
+
+            cmd = new SqlCommand
+            {
+                Connection = mSqlConn1,
+                CommandText = sqlSelect + sqlWhere
+            };
+
+            foreach (DataRow row in dt.Rows)
+            {
+                currentRowCount += 1;
+
+                colName = "Smry Trns Typ Txt";
+                valueParsed = ParseColumn(row, colName);
+                if (valueParsed.Equals("Gross Flows"))
+                {
+                    colName = "AssetManager";
+                    valueParsed = ParseColumn(row, colName);
+                    if (currentRowCount == 2)
+                    {
+                        cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                        valueParsed = ParseColumn(row, colName);
+                        cmd.Parameters["@" + colName].Value = valueParsed;
+                    }
+
+                    colName = "SponsorFirm";
+                    valueParsed = ParseColumn(row, colName);
+                    if ((valueParsed.Length > 0) && (valueParsed.EndsWith("Total") == false))
+                        sponsorFirm = valueParsed;
+                    if (currentRowCount == 2) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                    cmd.Parameters["@" + colName].Value = sponsorFirm;
+
+                    colName = "SmaStrategy";
+                    valueParsed = ParseColumn(row, colName);
+                    if (valueParsed.Length > 0)
+                        smaStrategy = valueParsed;
+                    if (currentRowCount == 2) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                    cmd.Parameters["@" + colName].Value = smaStrategy;
+
+                    colName = "SmaProductType";
+                    valueParsed = ParseColumn(row, colName);
+                    if (valueParsed.Length > 0)
+                        smaProductType = valueParsed;
+                    if (currentRowCount == 2) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                    cmd.Parameters["@" + colName].Value = smaProductType;
+
+                    colName = "ManagerClass";
+                    valueParsed = ParseColumn(row, colName);
+                    if (currentRowCount == 2) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                    cmd.Parameters["@" + colName].Value = valueParsed;
+
+                    try
+                    {
+                        cmd.CommandText = sqlSelect + sqlWhere;
+                        int iCount = (int)cmd.ExecuteScalar();
+                        if (iCount == 0)
+                        {
+                            colName = "MorningstarStrategyId";
+                            valueParsed = ParseColumn(row, colName);
+                            if (valueParsed.Length > 0)
+                                morningstarStrategyId = valueParsed;
+                            if (addCount == 0) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                            cmd.Parameters["@" + colName].Value = morningstarStrategyId;
+
+                            colName = "MorningstarClass";
+                            valueParsed = ParseColumn(row, colName);
+                            if (valueParsed.Length > 0)
+                                morningstarClass = valueParsed;
+                            if (addCount == 0) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                            cmd.Parameters["@" + colName].Value = morningstarClass;
+
+                            //colName = "TotalAccounts";
+                            //valueParsed = ParseColumn(row, colName);
+                            //if (addCount == 0) cmd.Parameters.Add("@" + colName, SqlDbType.VarChar);
+                            //cmd.Parameters["@" + colName].Value = valueParsed;
+
+                            colName = "CsvFileRow";
+                            if (addCount == 0) cmd.Parameters.Add("@" + colName, SqlDbType.Int);
+                            cmd.Parameters["@" + colName].Value = currentRowCount;
+
+                            // In addition to the key fields above, Principal does not provide the following non key fields so they are removed
+                            //    @SponsorFirmId, @MorningstarClassId, @TotalAccounts,  Note @TotalAccounts is provided but is not on the Offering level but on the Sponsor Firm level
+                            cmd.CommandText =
+                            "insert into " + tableName +
+                                "(AssetManager, SponsorFirm, SmaStrategy, SmaProductType," +
+                                " MorningstarStrategyId, MorningstarClass, ManagerClass, CsvFileRow) " +
+                            "Values (@AssetManager, @SponsorFirm, @SmaStrategy, @SmaProductType, " +
+                                    "@MorningstarStrategyId, @MorningstarClass, @ManagerClass, @CsvFileRow)";
+                            cmd.ExecuteNonQuery();
+                            addCount += 1;
+                        }
+                        else if (iCount > 0)
+                        {
+                            LogHelper.WriteLine("----- Skipping Row " + (currentRowCount) + "------");
+
+                            foreach (DataColumn column in dt.Columns)
+                            {
+                                if (column.ColumnName.Equals("AssetManager") || column.ColumnName.Equals("SponsorFirm") || column.ColumnName.Equals("AdvisoryPlatform")
+                                    || column.ColumnName.Equals("SmaStrategy") || column.ColumnName.Equals("SmaProductType"))
+                                    LogHelper.WriteLine(column.ColumnName.ToString() + "|" + row[column].ToString());
+                            }
+                            LogHelper.WriteLine("-----------");
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        LogHelper.WriteLine(logFuncName + ex.Message + " line number: " + ex.LineNumber);
+                    }
+                    finally
+                    {
+                    }
                 }
             }
             LogHelper.WriteLine(logFuncName + "Rows Processed " + currentRowCount);
